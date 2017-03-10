a725f9e2464220fd1451d1acfcc235762e3e6db7
Chris Guzman <guzmantchris@gmail.com>
2017-03-10 14:43:27 -0500

    Implement phone lookup functionality
    Add phone lookup form
    Add Nexmo.lookup() method

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require_relative 'nexmo.rb'
+
 class CallerId < Sinatra::Base
   get '/' do
     erb :phone_form
   end
+
+  post '/lookup' do
+		@insight = Nexmo.lookup(params["phone"], "#{request.base_url}/nexmo_insights?email=#{params["email"]}")
+		erb	:phone_lookup
+	end
 
   CallerId.run!
 end
